035a68736d3d041208e2c374aef8e352a997b1b9
Ahmad Salhi <asalhi@asaltech.com>
2020-04-02 15:01:39 +0300

    change UnitTest1 to GameTests.

--- TestRockPaperScissor.UnitTests/GameTests.cs
+++ TestRockPaperScissor.UnitTests/GameTests.cs
@@ -1,20 +1,38 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperSCISSOR;
 
-namespace TestRockPaperSCISSOR.UnitTests
+namespace TestRockPaperScissor.UnitTests
 {
     [TestClass]
     public class GameTests
     {
         [TestMethod]
-        public void checkRoundWinner_firstPlayerWins_returnsTrue()
+        public void checkRoundWinner_player1Wins_returnsTrue()
         {
-            //Arange
-            var player1 = new Player("hhhhh");
+            // Act
+            var result1 = Game.checkRoundWinner((int)TurnType.ROCK, (int)TurnType.SCISSOR);
+            var result2 = Game.checkRoundWinner((int)TurnType.PAPER, (int)TurnType.ROCK);
+            var result3 = Game.checkRoundWinner((int)TurnType.SCISSOR, (int)TurnType.PAPER);
 
-            //Act
+            // Assert
+            Assert.AreEqual(result1, true);
+            Assert.AreEqual(result2, true);
+            Assert.AreEqual(result3, true);
+        }
 
-            //Assert
+        [TestMethod]
+        public void checkRoundWinner_player1Loses_returnsFalse()
+        {
+            // Act
+            var result1 = Game.checkRoundWinner((int)TurnType.ROCK, (int)TurnType.PAPER);
+            var result2 = Game.checkRoundWinner((int)TurnType.PAPER, (int)TurnType.SCISSOR);
+            var result3 = Game.checkRoundWinner((int)TurnType.SCISSOR, (int)TurnType.ROCK);
+
+            // Assert
+            Assert.AreEqual(result1, false);
+            Assert.AreEqual(result2, false);
+            Assert.AreEqual(result3, false);
         }
     }
 }
